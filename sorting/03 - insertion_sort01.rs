362eb582b655b4c79fe6dbcbdaa03c3037a1ece3
daeikkim <12377dl@gmail.com>
2023-07-27 18:16:13 +0900

    insertion sort 1

--- sorting/03 - insertion_sort01.rs
+++ sorting/03 - insertion_sort01.rs
@@ -1,13 +1,32 @@
 use std::io::{self, BufRead};
 
 fn insertionSort1(n: i32, arr: &[i32]) {
-    // 정렬을 하려면 수정해야하므로 소유권을 이전해서 복사
     let mut vec = arr.to_owned();
-    // arr length
     let vec_size = vec.len();
-    // 배열의 마지막 값
     let last = arr[arr.len() - 1];
+
+    for i in 1..vec_size {
+        let mut pivot = vec
+        for j in (1..i + 1).rev() {
+            if vec[j - 1] <= vec[j] { break; }
+            vec.swap(j - 1, j);
+            for k in 0..vec_size {
+                print!("{} ", vec[k]);
+            }
+            println!();
+        }
+    }
 }
+
+// Your Output (stdout)
+// 2 4 6 3 8 
+// 2 4 3 6 8 
+// 2 3 4 6 8 
+// Expected Output
+// 2 4 6 8 8
+// 2 4 6 6 8
+// 2 4 4 6 8
+// 2 3 4 6 8
 
 fn main() {
     let stdin = io::stdin();

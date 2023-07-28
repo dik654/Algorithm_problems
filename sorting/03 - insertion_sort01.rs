fef2bb02e1176f75cf7a7e3b6058652cc357c295
daeikkim <12377dl@gmail.com>
2023-07-28 10:43:34 +0900

    insertion sort1

--- sorting/03 - insertion_sort01.rs
+++ sorting/03 - insertion_sort01.rs
@@ -5,28 +5,40 @@ fn insertionSort1(n: i32, arr: &[i32]) {
     let vec_size = vec.len();
     let last = arr[arr.len() - 1];
 
-    for i in 1..vec_size {
-        let mut pivot = vec
-        for j in (1..i + 1).rev() {
-            if vec[j - 1] <= vec[j] { break; }
-            vec.swap(j - 1, j);
-            for k in 0..vec_size {
-                print!("{} ", vec[k]);
+    for i in 0..vec_size {
+        let mut printer: bool = false;
+        let pivot: i32 = vec[i];
+        for j in (1..i+1).rev() {
+            if vec[j-1] <= pivot { 
+                vec[j] = pivot;
+                if printer {   
+                    vec.iter().for_each(|element|
+                        print!("{} ", element)
+                    );
+                    println!();
+                }
+                break;
             }
-            println!();
+            if vec[j-1] > pivot {
+                printer = true;
+                vec[j] = vec[j-1];
+                if printer {   
+                    vec.iter().for_each(|element|
+                        print!("{} ", element)
+                    );
+                    println!();
+                }
+                if i == vec_size-1 && j == 1 && vec[j-1] > pivot {
+                    vec[j-1] = pivot;
+                    vec.iter().for_each(|element|
+                        print!("{} ", element)
+                    );
+                    println!();
+                }
+            }
         }
     }
 }
-
-// Your Output (stdout)
-// 2 4 6 3 8 
-// 2 4 3 6 8 
-// 2 3 4 6 8 
-// Expected Output
-// 2 4 6 8 8
-// 2 4 6 6 8
-// 2 4 4 6 8
-// 2 3 4 6 8
 
 fn main() {
     let stdin = io::stdin();

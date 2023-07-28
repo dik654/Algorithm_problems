3ba7719134e8630646122f22d98d547363d7b3b8
daeikkim <12377dl@gmail.com>
2023-07-28 10:45:26 +0900

    insertion sort1

--- sorting/03 - insertion_sort01.rs
+++ sorting/03 - insertion_sort01.rs
@@ -1,39 +1,35 @@
 use std::io::{self, BufRead};
 
+fn print_vec(flag: bool, vec: &Vec<i32>) {
+    if flag == true {
+        vec.iter().for_each(|element|
+            print!("{} ", element)
+        );
+        println!();
+    }
+}
+
 fn insertionSort1(n: i32, arr: &[i32]) {
     let mut vec = arr.to_owned();
     let vec_size = vec.len();
     let last = arr[arr.len() - 1];
 
     for i in 0..vec_size {
-        let mut printer: bool = false;
+        let mut flag: bool = false;
         let pivot: i32 = vec[i];
         for j in (1..i+1).rev() {
             if vec[j-1] <= pivot { 
                 vec[j] = pivot;
-                if printer {   
-                    vec.iter().for_each(|element|
-                        print!("{} ", element)
-                    );
-                    println!();
-                }
+                print_vec(flag, &vec);
                 break;
             }
             if vec[j-1] > pivot {
-                printer = true;
+                flag = true;
                 vec[j] = vec[j-1];
-                if printer {   
-                    vec.iter().for_each(|element|
-                        print!("{} ", element)
-                    );
-                    println!();
-                }
+                print_vec(flag, &vec);
                 if i == vec_size-1 && j == 1 && vec[j-1] > pivot {
                     vec[j-1] = pivot;
-                    vec.iter().for_each(|element|
-                        print!("{} ", element)
-                    );
-                    println!();
+                    print_vec(flag, &vec);
                 }
             }
         }
